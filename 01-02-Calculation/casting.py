350904b75ad47efff6a39ba3c82d10762101dc66
Praxiskecee <ghatfankalinggasch@gmail.com>
2024-07-30 11:35:00 +0700

    learn casting

---
+++ 01-02-Calculation/casting.py
@@ -0,0 +1,8 @@
+# Casting ... datatypes ... (.. Value ..)
+
+x = "3.75"
+y = "4.0"
+
+result = (float(x) + float(y)) 
+
+print(result)
---
+++ 01-02-Calculation/string.py
@@ -0,0 +1,5 @@
+word = 'helo'
+word2 = "hello kak Ali"
+
+print(word2[4])
+print(len(word2))
