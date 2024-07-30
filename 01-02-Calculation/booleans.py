2d08e2f292ba6612a15013004ed11292718fa2ef
Praxiskecee <ghatfankalinggasch@gmail.com>
2024-07-30 13:37:16 +0700

    learn operators

---
+++ 01-02-Calculation/booleans.py
@@ -0,0 +1,16 @@
+# a = 400
+# b = 300
+
+# if b > a:
+#     print("b lebih kecil dari pada a")
+# else:
+#     print("a lebih besar dari pada b")
+
+print(5 > 3)
+print(5 < 3)
+
+bool_1 = True
+bool_2 = False
+
+print(bool_1 == bool_2)
+print(bool_1 != bool_2)
---
+++ 01-02-Calculation/operators.py
@@ -0,0 +1,19 @@
+a = 5
+b = 3 
+
+# a+=3 is same as a=a+3 
+
+# before
+print(a)
+print("First modulus: ", a % b)
+
+a += 4
+# after
+print(a)
+print("Second modulus: ", a % b)
+
+print(a**2)
+
+# floor division vs division 
+print("Floor division: ", 5 // b)
+print("division: ", 5 / 3)
